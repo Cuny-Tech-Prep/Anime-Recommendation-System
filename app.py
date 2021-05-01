028cfe11afa4c49c208b091bd526de8eeb829adb
Liulan Zheng <zhengliulan@gmail.com>
2021-05-01 18:26:41 -0400

    put everything in index.html

--- app.py
+++ app.py
@@ -45,14 +45,40 @@ for i in range(len(anime_pivot)):
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    
+    imgs =[]
+    title =[]
+    rating = []
+    synopsis =[]
     if flask.request.method == 'GET':
-        # Just render the initial form, to get input
-        return(flask.render_template('index.html'))
+        top5_animerating=anime_ratingCount[['anime_uid','title', 'score_review','img_url']].sort_values(by = 'score_review',ascending = False).head(5)
+        
+        for idd in top5_animerating['anime_uid']:
+            imgs.append((anime_ratingCount[anime_ratingCount['anime_uid']==idd]['img_url']).values[0])
+            synopsis.append((anime_full_data[anime_full_data['anime_uid']==idd]['synopsis']).values[0])
+            title.append((anime_full_data[anime_full_data['anime_uid']==idd]['title']).values[0])
+            rating.append((anime_full_data[anime_full_data['anime_uid']==idd]['score']).values[0])
+
+        return flask.render_template('index.html',
+            images = imgs, 
+            ratings = rating, 
+            synopsis = synopsis,
+            titles= title )
+
 
     if flask.request.method == 'POST':
+        top5_animerating=anime_ratingCount[['anime_uid','title', 'score_review','img_url']].sort_values(by = 'score_review',ascending = False).head(5)
+        
+        for idd in top5_animerating['anime_uid']:
+            imgs.append((anime_ratingCount[anime_ratingCount['anime_uid']==idd]['img_url']).values[0])
+            synopsis.append((anime_full_data[anime_full_data['anime_uid']==idd]['synopsis']).values[0])
+            title.append((anime_full_data[anime_full_data['anime_uid']==idd]['title']).values[0])
+            rating.append((anime_full_data[anime_full_data['anime_uid']==idd]['score']).values[0])
+
         results =[]
         user_input_text = flask.request.form['user_input_text']
         
+    
         if(isinstance(user_input_text, str)):
             # finds the id from the string.
             idx = arr.index(user_input_text)
@@ -61,37 +87,18 @@ def main():
         
         distances, indices = model_knn.kneighbors(anime_pivot.iloc[idx,:].values.reshape(1, -1), n_neighbors = N_RECOMMENDATIONS)
         for i in range(0, len(distances.flatten())):
-            if i == 0:
-                results.append(['Recommendations for {0} with id {1}:'.format(anime_pivot.index[idx], idx)])
-            else:
-                results.append(['{0}: {1}, with distance of {2}:'.format(i, anime_pivot.index[indices.flatten()[i]], round(distances.flatten()[i], 4))])
+            if i != 0:
+                results.append(anime_pivot.index[indices.flatten()[i]])
         
         return flask.render_template('index.html', 
             input_text=user_input_text,
-            result=results)
+            result=results,
+            images = imgs, 
+            ratings = rating, 
+            synopsis = synopsis,
+            titles= title 
+           )
            
-
-
-@app.route('/anime/')
-def anime():
-    top5_animerating=anime_ratingCount[['anime_uid','title', 'score_review','img_url']].sort_values(by = 'score_review',ascending = False).head(5)
-    imgs =[]
-    title =[]
-    rating = []
-    synopsis =[]
-    for idd in top5_animerating['anime_uid']:
-        imgs.append((anime_ratingCount[anime_ratingCount['anime_uid']==idd]['img_url']).values[0])
-        synopsis.append((anime_full_data[anime_full_data['anime_uid']==idd]['synopsis']).values[0])
-        title.append((anime_full_data[anime_full_data['anime_uid']==idd]['title']).values[0])
-        rating.append((anime_full_data[anime_full_data['anime_uid']==idd]['score']).values[0])
-
-    return flask.render_template('anime.html',
-        images = imgs, 
-        ratings = rating, 
-        synopsis = synopsis,
-        titles= title )
-
-
 
 
 if __name__ == '__main__':
